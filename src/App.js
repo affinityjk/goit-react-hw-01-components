b257961b759d797047de52cbf0c4f8c7126d6e2a
Julia Kutsyn <yuliyaflm@gmail.com>
2021-06-28 20:48:45 +0300

    adds source files

---
+++ src/App.js
@@ -0,0 +1,18 @@
+// import Profile from './components/SocialProfile/Profile';
+// import Statistics from './components/Statistics/Statistics';
+// import FriendList from './components/FriendList/FriendList';
+// import TransactionHistory from './components/TransactionHistory/TransactionHistory';
+// import user from './data/user.json';
+// import statisticsData from './data/statistical-data.json';
+// import friends from './data/friends.json';
+// import transactions from './data/transactions.json';
+
+const App = () => {
+  return (
+    <div>
+  
+    </div>
+  )
+}
+
+export default App;
