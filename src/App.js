13d9b3071eb843b538b73448207895b62218b21c
Julia Kutsyn <yuliyaflm@gmail.com>
2021-06-29 00:28:30 +0300

    adds images, Profile's markup, propTypes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
-// import Profile from './components/SocialProfile/Profile';
-// import Statistics from './components/Statistics/Statistics';
-// import FriendList from './components/FriendList/FriendList';
-// import TransactionHistory from './components/TransactionHistory/TransactionHistory';
-// import user from './data/user.json';
-// import statisticsData from './data/statistical-data.json';
-// import friends from './data/friends.json';
-// import transactions from './data/transactions.json';
+import Profile from './components/SocialProfile/Profile';
+import Statistics from './components/Statistics/Statistics';
+import FriendList from './components/FriendList/FriendList';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
+import user from './data/user.json';
+import statisticsData from './data/statistical-data.json';
+import friends from './data/friends.json';
+import transactions from './data/transactions.json';
 
 const App = () => {
   return (
     <div>
-  
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
+
+      {/* <Statistics title="Upload stats" stats={statisticsData} />;
+      <Statistics stats={statisticsData} />;
+
+      <FriendList friends={friends} />;
+
+      <TransactionHistory items={transactions} />; */}
     </div>
-  )
+  );
 }
 
 export default App;
---
+++ src/components/SocialProfile/Profile.js
@@ -0,0 +1,50 @@
+import PropTypes from 'prop-types';
+import defaultImg from '../../images/defaultImg.jpg';
+
+const Profile = ({ name, tag, location, avatar, followers, views, likes }) => {
+  return (
+    <div class="profile">
+      <div class="description">
+        <img
+          src={avatar}
+          alt="Аватар пользователя"
+          class={avatar}
+        />
+        <p class="name">{name}</p>
+        <p class="tag">{tag}</p>
+        <p class="location">{location}</p>
+      </div>
+
+      <ul class="stats">
+        <li>
+          <span class="label">Followers</span>
+          <span class="quantity">{followers}</span>
+        </li>
+        <li>
+          <span class="label">Views</span>
+          <span class="quantity">{views}</span>
+        </li>
+        <li>
+          <span class="label">Likes</span>
+          <span class="quantity">{likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+Profile.defaultProps = {
+  avatar: defaultImg,
+}
+
+Profile.propTypes = {
+  name: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string,
+  followers: PropTypes.number.isRequired,
+  views: PropTypes.number.isRequired,
+  likes:PropTypes.number.isRequired,
+}
+
+export default Profile;
